7ebf8edcca0922decec5cb41299bd623eab4f4ce
dipesh-commits <dipesh9393@gmail.com>
2020-06-19 16:01:11 +0545

    RGB color detection in real time

---
+++ color_detection.py
@@ -0,0 +1,95 @@
+#importing necessary libraries
+import cv2
+import numpy as np
+
+
+
+
+#Video capturing
+
+cap = cv2.VideoCapture(0)
+
+while True:
+	ret, frame = cap.read()
+	img = cv2.cvtColor(frame,cv2.COLOR_RGB2HSV)    #converting rgb image to hsv for masking
+
+
+	#assigning value for masking purpose for red channel
+	low_red = np.array([10,120,120])
+	high_red = np.array([180,255,255])					
+
+	red_mask = cv2.inRange(img,low_red,high_red)
+
+	final_red_img = cv2.bitwise_and(frame,frame,mask=red_mask)
+
+
+
+
+
+	#for green channel
+	low_green = np.array([36,50,40])
+	high_green = np.array([70,255,255])
+
+	green_mask = cv2.inRange(img,low_green,high_green)
+	green_mask = cv2.dilate(green_mask,kernel)
+
+	final_green_img = cv2.bitwise_and(frame,frame,mask=green_mask)
+
+
+
+
+	#for blue channel
+	low_blue = np.array([110,50,50])
+	high_blue = np.array([130,255,255])
+
+	blue_mask = cv2.inRange(img,low_blue,high_blue)
+
+	blue_mask = cv2.dilate(blue_mask,kernel)
+
+	final_blue_img = cv2.bitwise_and(frame,frame,mask=blue_mask)
+
+
+
+
+	#determining countours for real time detection
+
+
+	#for red channel
+	contours, hierarchy = cv2.findContours(red_mask,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+	for pic, contour in enumerate(contours):
+		area = cv2.contourArea(contour)
+		if area>1000:
+			x,y,w,h = cv2.boundingRect(contour)
+			frame = cv2.rectangle(frame,(x,y),(x+w,y+h),(0,0,255),4)
+			cv2.putText(frame,"Red color found",(x,y),cv2.FONT_HERSHEY_SIMPLEX,1.0,(0,0,255))
+
+
+
+	#for green channel
+	contours, hierarchy = cv2.findContours(green_mask,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+	for pic, contour in enumerate(contours):
+		area = cv2.contourArea(contour)
+		if area>1000:
+			x,y,w,h = cv2.boundingRect(contour)
+			frame = cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),4)
+			cv2.putText(frame,"Green color found",(x,y),cv2.FONT_HERSHEY_SIMPLEX,1.0,(0,255,0))
+
+
+	#for blue channel
+	contours, hierarchy = cv2.findContours(blue_mask,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+	for pic, contour in enumerate(contours):
+		area = cv2.contourArea(contour)
+		if area>1000:
+			x,y,w,h = cv2.boundingRect(contour)
+			frame = cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),4)
+			cv2.putText(frame,"Blue color found",(x,y),cv2.FONT_HERSHEY_SIMPLEX,1.0,(255,0,0))			
+
+
+	#showing respective frame
+	cv2.imshow('myimg',frame)
+	if cv2.waitKey(1) & 0xFF==ord('q'):
+		break
+
+cap.release()
+cv2.destroyAllWindows()
+
